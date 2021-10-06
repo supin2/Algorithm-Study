58a4b08e4874a485ca7762d3195c1bf2d5c87690
supin2 <supine94@gmail.com>
2021-10-06 22:48:19 +0900

    12847 solved

---
+++ baekjoon/SlidingWindow/12847.cpp
@@ -0,0 +1,27 @@
+// 백준 12847번 : 꿀 아르바이트
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+long long ans = 0;
+int arr[100001];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    
+    long long sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i > m) {
+            sum -= arr[i - m];
+        }
+        sum += arr[i];
+        ans = max(ans, sum);
+    }
+    cout << ans;
+    return 0;
+}
