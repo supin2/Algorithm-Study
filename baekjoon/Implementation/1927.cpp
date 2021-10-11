668de65832ebbc04c2a43004329bcbd904bc8b46
supin2 <supine94@gmail.com>
2021-10-11 22:50:52 +0900

    1927 solved

---
+++ baekjoon/Implementation/1927.cpp
@@ -0,0 +1,30 @@
+// 백준 1927번 : 최소 힙
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    int N;
+    cin >> N;
+    while (N--) {
+        int x;
+        cin >> x;
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+    return 0;
+}
