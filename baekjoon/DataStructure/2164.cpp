34b7a21918d01365ab2e5609415adb03cf023ba1
supin2 <supine94@gmail.com>
2021-10-15 23:27:26 +0900

    2164 solved

---
+++ baekjoon/DataStructure/2164.cpp
@@ -0,0 +1,25 @@
+// 백준 2164번 : 카드2
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    queue<int> q;
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        q.push(i);
+    }
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+    return 0;
+}
