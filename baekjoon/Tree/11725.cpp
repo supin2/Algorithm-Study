7718c35c2d33dd0158f6deb1a8ea769bea4ed711
supin2 <supine94@gmail.com>
2021-10-15 21:46:09 +0900

    11725 solved

--- baekjoon/Tree/11725.cpp
+++ baekjoon/Tree/11725.cpp
@@ -5,8 +5,25 @@
 using namespace std;
 
 vector<int> v[100001];
+int parent[100001];
+
+void dfs(int node) {
+    if (v[node].empty()) {
+        return;
+    }
+
+    for (int i = 0; i < v[node].size(); i++) {
+        int next = v[node][i];
+        if (!parent[next]) {
+            parent[next] = node;
+            dfs(next);
+        }
+    }
+}
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     int N;
     cin >> N;
 
@@ -16,6 +33,11 @@ int main() {
         v[a].push_back(b);
         v[b].push_back(a);
     }
+    parent[1] = 1;
+    dfs(1);
 
+    for (int i = 2; i <= N; i++) {
+        cout << parent[i] << '\n';
+    }
     return 0;
 }
