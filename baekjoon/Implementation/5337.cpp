b6b8a240e431ed3bf36674770cb62ced18fd3d28
supin2 <supine94@gmail.com>
2021-10-17 15:28:45 +0900

    cout problem solved

---
+++ baekjoon/Implementation/5337.cpp
@@ -0,0 +1,11 @@
+// 백준 5337번 : 웰컴
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << ".  .   ." << '\n';
+    cout << "|  | _ | _. _ ._ _  _" << '\n';
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+    return 0;
+}
