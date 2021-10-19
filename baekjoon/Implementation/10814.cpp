a038be97fa4a32d46c1196ccc46891cd6f30b3c9
supin2 <supine94@gmail.com>
2021-10-20 00:19:14 +0900

    10814 solved

---
+++ baekjoon/Implementation/10814.cpp
@@ -0,0 +1,30 @@
+// 백준 10814번 : 나이순 정렬
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(pair<int, string> p1, pair<int, string> p2) {
+    return p1.first < p2.first;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, string>> v;
+    while (N--) {
+        int age;
+        string name;
+        cin >> age >> name;
+        v.push_back(make_pair(age, name));
+    }
+    stalbe_sort(v.begin(), v.end(), cmp);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i].first << ' ' << v[i].second << '\n';
+    }
+    return 0;
+}
