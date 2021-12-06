6a4e0a500b6a477841d96ada9ce2017cae316fc3
supin2 <supine94@gmail.com>
2021-12-07 00:24:45 +0900

    2530 solved

--- baekjoon/Mathematics/2530.py
+++ baekjoon/Mathematics/2530.py
@@ -1,6 +1,27 @@
 # 백준 2530번 : 인공지능 시계
 
+def calc(HH, MM, SS, T) :
+    SS += T % 60
+    T = T // 60
+    if SS >= 60:
+        SS -= 60
+        MM += 1
+
+    MM += T % 60
+    T = T // 60
+    if MM >= 60:
+        MM -= 60
+        HH += 1
+
+    HH += T % 24
+    if HH >= 24:
+        HH -= 24
+
+    print(HH, MM, SS)
+
+
 if __name__ == "__main__" :
     HH, MM, SS = map(int, input().split())
     time = int(input())
+    calc(HH, MM, SS, time)
 
