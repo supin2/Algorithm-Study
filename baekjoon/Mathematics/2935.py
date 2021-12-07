cef2c31e93f29ead515eed1e7f06b6314ce2721e
supin2 <supine94@gmail.com>
2021-12-08 00:22:48 +0900

    2935 solved

---
+++ baekjoon/Mathematics/2935.py
@@ -0,0 +1,10 @@
+# 백준 2935번 : 소음
+if __name__ == "__main__":
+    opnd1 = int(input())
+    optr = input()
+    opnd2 = int(input())
+
+    if optr == '+':
+        print(opnd1 + opnd2)
+    else:
+        print(opnd1 * opnd2)
