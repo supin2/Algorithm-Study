24ff26e4cf52f19a2341525fafe567194d9d8145
supin2 <supine94@gmail.com>
2022-01-30 23:24:25 +0900

    Solved problem using python

---
+++ leetcode/Easy/21. Merge Two Sorted Lists.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mergeTwoLists(self, a, b):
+        if a and b:
+            if a.val > b.val:
+                a, b = b, a
+            a.next = self.mergeTwoLists(a.next, b)
+        return a or b
+
+# Maybe it is the best solution
